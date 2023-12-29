ae26d3eaf836ff94b5a28df70413c3f5dfaff5bb
ashay-thamankar <ashaythamankar@gmail.com>
2023-12-29 17:10:12 +0530

    app updated

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def predict_datapoint():
         return render_template('home.html', results= round(results[0],2))
     
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0",port= 8080)
